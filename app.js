adb5a8358a62ba080c7b66b105d5b1c49f7fa48a
maghaei <majid.aghaei1363@gmail.com>
2017-12-19 00:11:35 +0200

    Assignment is done.

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 (function () {
+  'use strict';
   angular.module('eatApp', [])
-  .controller('eatAppController', $Scope);
+  .controller('eatAppController', function ($scope) {
+    $scope.items="";
+    $scope.message="";
+    $scope.checkItems = function () {
+      var eatItems = $scope.items;
+      if (eatItems=="") $scope.message = "Please enter data first!";
+      else {
+        var numberOfItems = parsString(eatItems, ',');
+        if (numberOfItems <= 3)
+        $scope.message = "Enjoy!";
+        else {
+          $scope.message = "Too Much!";
+        }
+      }
+    };
+
+    function parsString(stringToParse, separator) {
+      var itemsArray = stringToParse.split(separator);
+      return itemsArray.length;
+    };
+  });
 })();
